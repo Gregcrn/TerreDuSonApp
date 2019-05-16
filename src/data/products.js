1c9c817d303fc45124f0d7d3e97fbb1625dc4183
GregCrn <gcorin65@gmail.com>
2019-05-16 16:18:25 +0200

    udapte single quote  and localhost

--- src/data/products.js
+++ src/data/products.js
@@ -1,5 +1,5 @@
 const request = new XMLHttpRequest();
-request.open("GET", 'http://localhost:8888/API-TerresDuSon/product/productlist.php', false);
+request.open('GET', 'http://192.168.1.165:8888/API-TerresDuSon/product/productlist.php', false);
 request.send();
 
 export default JSON.parse(request.responseText);
--- src/data/users.js
+++ src/data/users.js
@@ -1,9 +1,5 @@
 const request = new XMLHttpRequest();
-<<<<<<< HEAD
 request.open('GET', 'http://192.168.1.165:8888/API-TerresDuSon/user/userlist.php', false);
-=======
-request.open("GET", 'http://192.168.1.165:8888/API-TerresDuSon/user/userlist.php', false);
->>>>>>> PreProd
 request.send();
 
 export default JSON.parse(request.responseText);
